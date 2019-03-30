64970b4d11041cf11bea37abff25de6d04c83f15
Hunter Parcells <hparcells@icloud.com>
2019-03-30 17:31:38 -0400

    Hands and New Restart Rules

--- index.js
+++ index.js
@@ -19,25 +19,35 @@ let game = {
     czar: 0
   }
 }
+let started = false;
 
 // Start.
 IO.on('connection', (client) => {
   client.on('newPlayer', (username) => {
     game.players.push({
       username: username,
-      score: 0
+      score: 0,
+      hand: []
     });
+
+    // Update the client's states.
     IO.emit('updatedGame', game);
+    console.log(`Player ${username} connected.`);
   });
   client.on('playerDisconnect', (username) => {
     // Remove player.
     game.players.splice(game.players.findIndex((x) => x.username === username), 1);
     IO.emit('updatedGame', game);
+    console.log(`Player ${username} disconnected.`);
 
-    // If everyone is gone.
-    if(game.players.length === 0) {
-      game = defaultGame;
+    // If there is not enough people to join.
+    if(game.players.length < 4 && started) {
+      resetGame();
     }
   });
 });
 SERVER.listen(PORT);
+
+function resetGame() {
+  game = defaultGame;
+}
